bd312602e369781739b460b2f2be5c922412786e
Alper Ünlü <alperunlu@ieee.org>
2024-03-19 13:38:41 +0100

    Update show.php

--- bolrozet/show.php
+++ bolrozet/show.php
@@ -37,14 +37,15 @@ if ($rozet>=10000000000) $argeci = substr($rozet, -11, 1);
 ?>
 
 
-
-  <table>
-  <thead>
-    <tr>
-      <th><? if($verified=="1"){ echo "<img src=\"https://cdn2.iconfinder.com/data/icons/essentials-volume-i/128/verified-gold-512.png\" title=\"onaylanmış hesap\" width=32 height=32> <font size=1>";}?> 
-        <? if($kimse){ echo "<a href=\"sozluk.php?process=word&q=$kimdirbu\" title=\"$kimdirbu\" target=main><font size=1>$kimdirbu </A> -  $profilinfo";}else{echo "böyle biri yok";die;exit;}?>
-        <? if($verified=="1"){ echo "<img src=\"https://cdn2.iconfinder.com/data/icons/essentials-volume-i/128/verified-gold-512.png\" title=\"onaylanmış hesap\" width=32 height=32> <font size=1>";}?>
-<?if (($rozet!="0") and ($rozet!="000000000000")) {?>
+<table>
+<thead>
+<tr>
+<th>
+  
+<? if($verified=="1"){ echo "<img src=\"https://cdn2.iconfinder.com/data/icons/essentials-volume-i/128/verified-gold-512.png\" title=\"onaylanmış hesap\" width=32 height=32> <font size=1>";}?> 
+<? if($kimse){ echo "<a href=\"sozluk.php?process=word&q=$kimdirbu\" title=\"$kimdirbu\" target=main><font size=1>$kimdirbu </A> -  $profilinfo";}else{echo "böyle biri yok";die;exit;}?>
+<? if($verified=="1"){ echo "<img src=\"https://cdn2.iconfinder.com/data/icons/essentials-volume-i/128/verified-gold-512.png\" title=\"onaylanmış hesap\" width=32 height=32> <font size=1>";}?>
+<? if (($rozet!="0") and ($rozet!="000000000000")) {?>
 
 <br><br><? if($imece=="1"){ echo "<img src=\"http://www.bolsozluk.com/bolrozet/imececi.png\" title=\"imececi\" width=32 height=32> <font size=1>";}
 if($gececi=="1"){ echo "<img src=\"http://www.bolsozluk.com/bolrozet/gececi.png\" title=\"gece tayfası\" width=32 height=32> <font size=1>";}
@@ -53,39 +54,5 @@ if($bolyazar=="1"){ echo "<img src=\"http://www.bolsozluk.com/bolrozet/bolyazar.
 if($solfc=="1"){ echo "<img src=\"http://www.bolsozluk.com/bolrozet/solfc.png\" title=\"sol frame canavarı\" width=32 height=32> <font size=1>";}
 if($argeci=="1"){ echo "<img src=\"http://www.bolsozluk.com/bolrozet/argeci.png\" title=\"argeci\" width=32 height=32> <font size=1>";}
 }
-
-/*
-<?php
-$rozet = 11100100011;
-
-$imece = 0;
-$gececi = 0;
-$sevilen = 0;
-$bolyazar = 0;
-$solfc = 0;
-$argeci = 0;
-
-if ($rozet>=1) $imece = substr($rozet, -1, 1); 
-if ($rozet>=10) $gececi = substr($rozet, -2, 1);
-if ($rozet>=100000) $sevilen = substr($rozet, -6, 1); 
-if ($rozet>=100000000) $bolyazar = substr($rozet, -9, 1); 
-if ($rozet>=1000000000) $solfc = substr($rozet, -10, 1); 
-if ($rozet>=10000000000) $argeci = substr($rozet, -11, 1);
-
-echo "<br>imece: ";
-echo $imece;
-echo "<br> gececi: ";
-echo $gececi;
-echo "<br> sevilen: ";
-echo $sevilen;
-echo "<br> bolyazar: ";
-echo $bolyazar;
-echo "<br> solfc: ";
-echo $solfc;
-echo "<br> argeci: ";
-echo $argeci;
-
-?>
-*/
 
 ?>
